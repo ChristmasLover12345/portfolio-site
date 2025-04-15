b2ecbba6d35fb3299cd227fb1562f0940bf09d67
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-14 20:19:33 -0700

    forgot interface

---
+++ src/utils/interface.ts
@@ -0,0 +1,15 @@
+export interface MediaCardProps {
+  background: string;
+  imageOnRight: boolean;
+  LogoUrl: string;
+  LogoSize: string;
+  Description: string;
+  textColor: string;
+  ButtonColor: string;
+  cardTitle: string;
+  cardImageUrl: string;
+  cardVideoUrl: string;
+  cardAudioUrl: string;
+  LearnMoreUrl: string;
+  CompanyLink: string;
+}
