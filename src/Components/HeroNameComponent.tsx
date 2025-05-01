196acecdb846fde9ecd79fc619baccae528d489c
ChristmasLover12345 <madnes0205@gmail.com>
2025-05-01 10:49:02 -0700

    changing stuff up

---
+++ src/Components/HeroNameComponent.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const HeroNameComponent = () => {
+  return (
+    <div>HeroNameComponent</div>
+  )
+}
+
+export default HeroNameComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import HeroImageComponent from "@/Components/HeroImageComponent";
+
 
 
 
 export default function Home() {
   return (
 
-    <div className="min-h-screen min-w-screen bg-gray-900">
+    <div className="min-h-screen min-w-screen bg-sky-950 flex flex-col items-center justify-center p-4">
 
-     <HeroImageComponent />
+     
 
       
     
