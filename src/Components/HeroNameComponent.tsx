ca9d8cea44a3007b161c9260e02a69b72956bbe2
ChristmasLover12345 <madnes0205@gmail.com>
2025-05-02 11:25:19 -0700

    ocean theme

--- src/Components/HeroNameComponent.tsx
+++ src/Components/HeroNameComponent.tsx
@@ -8,17 +8,19 @@ const HeroNameComponent = () => {
 
    
 
-    <div className='w-full h-[400px] bg-gradient-to-b from-sky-700 to-blue-950 flex flex-col justify-center items-center text-white p-4 '>
+    <div className='w-full h-[600px] flex justify-center items-center bg-white/5 backdrop-blur-md border border-white/10 rounded-2xl shadow-lg p-8 innerGlow'>
 
  
       <motion.div 
       initial={{ opacity: 0, y: 30}} //Starter point, where the animation start
       animate={{opacity: 1, y:0}} //edning point
       transition={{duration: 1}} //Duration of the animation
-      className='w-full h-full flex flex-col justify-center items-center'>
+      className='  flex flex-col justify-center items-center
+          text-white text-center
+          w-full max-w-4xl'>
 
-            <h3 className='text-4xl md:text-6xl font-bold'>Welcome! my name is Juan Heredia.</h3>
-            <h1 className='text-2xl md:text-4xl font-bold mt-2'>I am a Full Stack Developer</h1>
+            <h3 className='text-4xl md:text-6xl font-bold mb-4 drop-shadow-md'>Welcome! my name is Juan Heredia.</h3>
+            <h1 className='text-2xl md:text-4xl text-sky-200 font-bold mt-2 drop-shadow-sm'>I am a Full Stack Developer</h1>
 
       </motion.div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
 
     <div className=" relative min-h-screen min-w-screen  flex flex-col overflow-hidden">
 
-<div className="absolute inset-0 -z-10 bg-blue-950">
+<div className="absolute inset-0 -z-10 bg-gradient-to-b from-[#0f172a] via-[#1e293b] to-[#1e3a8a]">
     <div className="relative w-full h-full overflow-hidden">
-      {[...Array(20)].map((_, i) => (
+      {[...Array(25)].map((_, i) => (
         <span key={i} className="bubble" style={{
           left: `${Math.random() * 100}%`,
-          animationDelay: `${Math.random() * -10}s`,
-          width: `${10 + Math.random() * 20}px`,
-          height: `${10 + Math.random() * 20}px`
+          animationDelay: `${Math.random() * -20}s`,
+          width: `${20 + Math.random() * 40}px`,
+          height: `${20 + Math.random() * 40}px`,
         }} ></span>
       ))}
     </div>
